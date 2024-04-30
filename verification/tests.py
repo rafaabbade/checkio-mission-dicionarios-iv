693b32a5690011ee77b776d98a8bc8fe95f2092c
Rafael Abbade <rafaelvabbade@gmail.com>
2024-04-30 10:45:59 -0300

    Initial commit from template

--- verification/tests.py
+++ verification/tests.py
@@ -10,6 +10,20 @@ Each test is a dict with
 
 TESTS = {
     "Basics": [
-        {{TESTES}}
+            {
+        "input": [["Fahrenheit 451", "Ray Bradbury"], ["To Kill a Mockingbird", "Harper Lee"]],
+        "answer": {
+            "livro1": {"titulo": "Fahrenheit 451", "autor": "Ray Bradbury"},
+            "livro2": {"titulo": "To Kill a Mockingbird", "autor": "Harper Lee"}
+        }
+    },
+    {
+        "input": [["The Great Gatsby", "F. Scott Fitzgerald"], ["The Catcher in the Rye", "J.D. Salinger"], ["1984", "George Orwell"]],
+        "answer": {
+            "livro1": {"titulo": "The Great Gatsby", "autor": "F. Scott Fitzgerald"},
+            "livro2": {"titulo": "The Catcher in the Rye", "autor": "J.D. Salinger"},
+            "livro3": {"titulo": "1984", "autor": "George Orwell"}
+        }
+    }
     ]
 }
